3a4eba5fa9a8d969583145156386324ed51279d1
pan.mingzhi <pmz1321@163.com>
2013-07-29 23:45:35 +0800

    modify

--- hs_code/2013_07/quick_sort.hs
+++ hs_code/2013_07/quick_sort.hs
@@ -1,5 +1,6 @@
 quick_sort :: (Ord a) => [a] -> [a]
 quick_sort [] = []
+quick_sort [x] = [x]
 quick_sort (x,lx) = 
 	let left_part = quick_sort[a | a <- lx, a <= x]
 	    right_part = quick_sort[a | a <- lx, a > x]
