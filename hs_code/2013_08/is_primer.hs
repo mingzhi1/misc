de4ec20185c0cf09d876b324efc55559d1da3299
panmingzhi <panmingzhi>
2013-08-06 09:46:00 +0800

    settt

--- hs_code/2013_08/is_primer.hs
+++ hs_code/2013_08/is_primer.hs
@@ -1,7 +1,7 @@
 is_prime :: Int -> Bool
 is_prime n
-	| n < 2 		= False
-	| n == 2 		= True
+	| n < 2				= False
+	| n == 2			= True
 	| (n `mod` 2) == 0 	= False
-	| otherwise 		= all (( /=0 ).(mod n)) (takeWhile (\x -> x*x <= n) [3,5..])
+	| otherwise			= all (( /=0 ).(mod n)) (takeWhile (\x -> x*x <= n) [3,5..])
 
